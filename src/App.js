6008100c58b4c86171e40fa0fd1e2c76f486a727
Belkis Barrios <belkis.e.barrios@gmail.com>
2023-01-06 15:54:39 -0600

    Removed unnecessary files

--- src/App.js
+++ src/App.js
@@ -2,15 +2,6 @@ import React from "react";
 import Weather from "./Weather";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Weather App</h1>
-        <Weather />
-      </header>
-    </div>
-  );
+export default function App() {
+  return <div className="App">Weather App</div>;
 }
-
-export default App;
