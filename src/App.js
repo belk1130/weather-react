bea560f69c65e2d06b1dc8327e0b16c7f8f3487c
Belkis Barrios <belkis.e.barrios@gmail.com>
2023-01-06 16:28:29 -0600

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,5 +3,17 @@ import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Belkis Barrios and is{" "}
+          <a href="https://github.com/belk1130/weather-react" target="_blank">
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
 }
